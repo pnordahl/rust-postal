a9976b4715ada455a1292171e7bd1efd0e392702
Patrick Nordahl <plnordahl@gmail.com>
2019-07-11 23:18:42 -0500

    wip for parse_address support

--- benches/benches.rs
+++ benches/benches.rs
@@ -5,22 +5,44 @@ extern crate criterion;
 
 use criterion::black_box;
 use criterion::Criterion;
-use postal::{Context, ExpandAddressOptions, InitOptions};
+use postal::{Context, ExpandAddressOptions, InitOptions, ParseAddressOptions};
 
 fn criterion_benchmark(c: &mut Criterion) {
+    // expand_address
     let mut ctx = Context::new();
     ctx.init(InitOptions {
         expand_address: true,
+        parse_address: false,
     })
     .unwrap();
 
-    let mut opts = black_box(ExpandAddressOptions::new());
+    let mut opts = ExpandAddressOptions::new();
     opts.set_languages(vec!["en"].as_slice());
+    let mut bbopts = black_box(opts);
 
     c.bench_function("expand_address address 1", move |b| {
         let addr = black_box("1234 Cherry Ln Podunk TX");
         b.iter(|| {
-            ctx.expand_address(addr, &mut opts).unwrap();
+            ctx.expand_address(addr, &mut bbopts).unwrap();
+        })
+    });
+
+    // parse_address
+    println!("loading address parser...");
+    let mut ctx2 = Context::new();
+    ctx2.init(InitOptions {
+        expand_address: false,
+        parse_address: true,
+    })
+    .unwrap();
+    println!("done");
+
+    let opts2 = ParseAddressOptions::new();
+    let mut bbopts2 = black_box(opts2);
+    c.bench_function("parse_address address 1", move |b| {
+        let addr = black_box("1234 Cherry Ln Podunk TX");
+        b.iter(|| {
+            ctx2.parse_address(addr, &mut bbopts2).unwrap();
         })
     });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -42,8 +42,7 @@ pub struct Expansions<'a> {
     index: isize,
     array: *mut *mut c_char,
     array_length: isize,
-    // this is used to deal with the lifetime of the Item in our Iterator implementation;
-    // necessary to avoid the alloc
+    // this is used to deal with the lifetime of the Item in our Iterator implementation
     phantom: PhantomData<&'a str>,
 }
 impl<'a> Expansions<'a> {
@@ -88,13 +87,107 @@ impl<'a> Drop for Expansions<'a> {
     }
 }
 
+#[derive(Debug)]
+pub struct ParseAddressOptions {
+    pub opts: libpostal_address_parser_options_t,
+}
+impl ParseAddressOptions {
+    pub fn new() -> Self {
+        unsafe {
+            ParseAddressOptions {
+                opts: libpostal_get_address_parser_default_options(),
+            }
+        }
+    }
+    pub fn set_language(&mut self, language: &str) {
+        // TODO
+    }
+    pub fn set_country(&mut self, country: &str) {
+        // TODO
+    }
+}
+
+#[derive(Debug)]
+pub struct Components<'a> {
+    index: isize,
+    resp: *mut libpostal_address_parser_response_t,
+    // this is used to deal with the lifetime of the Item in our Iterator implementation
+    phantom: PhantomData<&'a Component<'a>>,
+}
+impl<'a> Components<'a> {
+    fn new(resp: *mut libpostal_address_parser_response_t) -> Self {
+        Components {
+            index: 0,
+            resp: resp,
+            phantom: PhantomData,
+        }
+    }
+}
+#[derive(Debug)]
+pub struct Component<'a> {
+    label: &'a str,
+    value: &'a str,
+}
+impl<'a> Iterator for Components<'a> {
+    type Item = Component<'a>;
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            let resp = *self.resp;
+            dbg!(self.index);
+            dbg!(resp.num_components);
+            if self.index < resp.num_components as isize {
+                let mut p = resp.components.offset(self.index);
+                let cs = CStr::from_ptr(p as *const c_char);
+                match cs.to_str() {
+                    Ok(component) => {
+                        dbg!(component);
+                        p = resp.labels.offset(self.index);
+                        let ls = CStr::from_ptr(p as *const c_char);
+                        match ls.to_str() {
+                            Ok(label) => {
+                                dbg!(label);
+                                self.index += 1;
+                                return Some(Component {
+                                    label: label,
+                                    value: component,
+                                });
+                            }
+                            Err(_) => {
+                                dbg!("label not a str");
+                                // TODO: bad string from libpostal, can't really handle
+                                return None;
+                            }
+                        }
+                    }
+                    Err(_) => {
+                        dbg!("component not a str");
+                        // TODO: bad string from libpostal, can't really handle
+                        return None;
+                    }
+                }
+            } else {
+                None
+            }
+        }
+    }
+}
+impl<'a> Drop for Components<'a> {
+    fn drop(&mut self) {
+        unsafe {
+            libpostal_address_parser_response_destroy(self.resp);
+        }
+    }
+}
+
 pub struct InitOptions {
     pub expand_address: bool,
+    pub parse_address: bool,
 }
 
 pub struct Context {
     setup_done: bool,
     expand_address_enabled: bool,
+    parse_address_enabled: bool,
     mutex: Mutex<bool>,
 }
 impl Context {
@@ -104,6 +197,7 @@ impl Context {
         Context {
             setup_done: false,
             expand_address_enabled: false,
+            parse_address_enabled: false,
             // this mutex is a sentinel for a global lock on all operations of this Context; the bool it
             // protects is unused.
             mutex: Mutex::new(false),
@@ -124,6 +218,14 @@ impl Context {
                 }
             }
             self.expand_address_enabled = true;
+        }
+        if opts.parse_address {
+            unsafe {
+                if !libpostal_setup_parser() {
+                    return Err(PostalError::LibpostalEnableParsing);
+                }
+            }
+            self.parse_address_enabled = true;
         }
         Ok(())
     }
@@ -150,6 +252,27 @@ impl Context {
             Err(PostalError::LibpostalNotReady)
         }
     }
+    pub fn parse_address(
+        &self,
+        a: &str,
+        opts: &mut ParseAddressOptions,
+    ) -> Result<Components, PostalError> {
+        if self.setup_done && self.parse_address_enabled {
+            let _ = self.mutex.lock();
+            unsafe {
+                match CString::new(a) {
+                    Ok(c_string) => {
+                        let addr = c_string.as_ptr() as *mut c_char;
+                        let raw = libpostal_parse_address(addr, opts.opts);
+                        Ok(Components::new(raw))
+                    }
+                    Err(e) => Err(PostalError::BadCString(e)),
+                }
+            }
+        } else {
+            Err(PostalError::LibpostalNotReady)
+        }
+    }
 }
 impl Drop for Context {
     fn drop(&mut self) {
@@ -163,6 +286,11 @@ impl Drop for Context {
                 libpostal_teardown_language_classifier();
             }
         }
+        if self.parse_address_enabled {
+            unsafe {
+                libpostal_setup_parser();
+            }
+        }
     }
 }
 
@@ -170,6 +298,7 @@ impl Drop for Context {
 pub enum PostalError {
     LibpostalSetup,
     LibpostalEnableExpansion,
+    LibpostalEnableParsing,
     BadCString(std::ffi::NulError),
     LibpostalNotReady,
 }
@@ -180,6 +309,7 @@ impl fmt::Display for PostalError {
             PostalError::LibpostalEnableExpansion => {
                 write!(f, "libpostal_setup_language_classifier failed")
             }
+            PostalError::LibpostalEnableParsing => write!(f, "libpostal_setup_parser failed"),
             PostalError::BadCString(ref err) => {
                 write!(f, "failed to convert &str into c string, error: '{}'", err)
             }
@@ -196,6 +326,7 @@ impl error::Error for PostalError {
         match *self {
             PostalError::LibpostalSetup => None,
             PostalError::LibpostalEnableExpansion => None,
+            PostalError::LibpostalEnableParsing => None,
             PostalError::BadCString(ref err) => Some(err),
             PostalError::LibpostalNotReady => None,
         }
@@ -207,10 +338,36 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_parse_address() {
+        println!("loading address parser...");
+        let mut ctx = Context::new();
+        ctx.init(InitOptions {
+            expand_address: false,
+            parse_address: true,
+        })
+        .unwrap();
+        println!("loading done");
+        let mut opts = ParseAddressOptions::new();
+        let components = ctx
+            .parse_address("1234 Bengal Ln, Plano TX 75023", &mut opts)
+            .unwrap();
+        for c in components {
+            dbg!(c);
+        }
+        // let expect = vec![
+        //     "thirty w 26th saint fleuve numero 7",
+        //     "thirty w 26 th saint fleuve numero 7",
+        // ];
+
+        // assert!(expansions.eq(expect));
+    }
+
+    #[test]
     fn test_expand_address() {
         let mut ctx = Context::new();
         ctx.init(InitOptions {
             expand_address: true,
+            parse_address: false,
         })
         .unwrap();
         let mut opts = ExpandAddressOptions::new();
@@ -231,6 +388,7 @@ mod tests {
         let mut ctx = Context::new();
         ctx.init(InitOptions {
             expand_address: true,
+            parse_address: false,
         })
         .unwrap();
         let mut opts = ExpandAddressOptions::new();
